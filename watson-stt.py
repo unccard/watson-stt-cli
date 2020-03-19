dd5fca83d6fdf86fb8f2611397547c27fdb3c43d
Ian Kim <me@ian.kim>
2020-03-19 14:00:31 -0400

    Use IBM Watson pypi package

--- watson-stt.py
+++ watson-stt.py
@@ -1,5 +1,16 @@
-import click
-import requests
+import json
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 if __name__ == "__main__":
-    pass
+    with open("ibm.json", "r") as configfile:
+        obj = json.load(configfile)
+        apikey, url = obj["apikey"], obj["url"]
+
+    with open("audio-file.flac", "rb") as audiofile:
+        authenticator = IAMAuthenticator(apikey)
+        speech_to_text = SpeechToTextV1(
+            authenticator=authenticator
+        )
+        speech_to_text.set_service_url(url)
+        response = speech_to_text.recognize(audiofile)
